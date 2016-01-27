214207ad0dab3c08921597895961f26ef1763589
gvialpai <vialpg@gmail.com>
2016-01-27 09:18:05 +0000

    Last commit before recreating project from scratch

---
+++ Dementia Citizens/Dementia Citizens/ContentViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContentViewController.swift
+//  Dementia Citizens
+//
+//  Created by Guillaume Vial-Pailler on 1/26/16.
+//  Copyright © 2016 Guillaume Vial-Pailler. All rights reserved.
+//
+
+import Cocoa
+
+class ContentViewController: UIView {
+
+}
